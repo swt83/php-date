fabb6bec564694a853fa3c76f7d8fa01e8beb880
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-04-25 21:35:37 -0300

    Adding localization files for brazilian portuguese and english.
    Adding localization files for brazilian portuguese and english.

---
+++ language/en/date.php
@@ -0,0 +1,19 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| /date Language Lines
+	|--------------------------------------------------------------------------
+	*/
+	'ago'		=> 'ago',
+	'second'	=> 'second',
+	'minute'	=> 'minute',
+	'hour'		=> 'hour',	
+	'day'		=> 'day',
+	'week'		=> 'week',
+	'month' 	=> 'month',
+	'year' 		=> 'year',
+	'decade'	=> 'decade',
+	);
---
+++ language/pt/date.php
@@ -0,0 +1,19 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| /date Language Lines
+	|--------------------------------------------------------------------------
+	*/
+	'ago'		=> 'atrás',
+	'second'	=> 'segundo',
+	'minute'	=> 'minuto',
+	'hour'		=> 'hora',	
+	'day'		=> 'dia',
+	'week'		=> 'semana',
+	'month' 	=> 'mês',
+	'year' 		=> 'ano',
+	'decade'	=> 'década',
+	);
